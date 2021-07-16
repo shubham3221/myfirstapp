94e1d7db9c8e8b6ea4f09e5426f10fca52ce78db
mindroot3 <mgupta@mind-roots.com>
2021-07-16 18:05:49 +0530

    updated

--- app/src/main/java/com/example/myfirstapp/paging/PagingActivity.kt
+++ app/src/main/java/com/example/myfirstapp/paging/PagingActivity.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myfirstapp.Myconstants.Companion.TAG
 import com.example.myfirstapp.databinding.ActivityPagingBinding
+import com.example.myfirstapp.utils.PaginationScrollListener
 import kotlin.collections.ArrayList
 
 class PagingActivity : AppCompatActivity() {
@@ -16,6 +17,9 @@ class PagingActivity : AppCompatActivity() {
     lateinit var viewModel: PagingViewModel
     lateinit var adapter: PagingRecycleAdapter
     var list = ArrayList<PagingDataClass>()
+    lateinit var linearLayoutManager: LinearLayoutManager
+    var isLastPage: Boolean = false
+    var isLoading: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,15 +35,36 @@ class PagingActivity : AppCompatActivity() {
     private fun setObserver() {
         viewModel.getPosts("https://reqres.in/api/users/1").observe(this, { data ->
             val g = ArrayList<PagingDataClass>()
-            g.add(data!!)
+            for (i in 0..20){
+                g.add(data!!)
+            }
             adapter.updateAdapter(g)
         })
     }
 
     private fun setupRecyclerview() {
-        binding.mRecycler.layoutManager = LinearLayoutManager(this)
+        linearLayoutManager = LinearLayoutManager(this)
+        binding.mRecycler.layoutManager =linearLayoutManager
         adapter = PagingRecycleAdapter(ArrayList())
         binding.mRecycler.adapter = adapter
+
+
+        binding.mRecycler.addOnScrollListener(object : PaginationScrollListener(linearLayoutManager) {
+            override fun isLastPage(): Boolean {
+                Log.e(TAG, "isLastPage: ")
+                return isLastPage
+            }
+
+            override fun isLoading(): Boolean {
+                Log.e(TAG, "isLoading: ")
+                return isLoading
+            }
+
+            override fun loadMoreItems() {
+
+            }
+
+        })
     }
 
     private fun setupViewmodel() {
